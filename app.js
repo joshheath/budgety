5dcc9046db1ad3157a3337e0fc8fff0338177054
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-10-04 13:27:47 +0100

    add event listeners for return and add click

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function() {
-  var x = 23;
 
-  var add = function(a){
-    return x + a;
-  }
 
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
 }());
 
+
+// UI CONTROLLER
 var UIController = (function() {
 
-  // code later
 
 }());
 
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
-  return {
-    anotherPublic: function(){
-      console.log(z);
-    }
+
+  var ctrlAddItem = function(){
+    // 1. Get field input data
+    // 2, Add item to the budget controller
+    // 3. Add items to UI
+    // 4. Calculate the budget
+    // 5. Display budget on UI
   }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event){
+    if(event.keyCode === 13 || event.which === 13){
+      ctrlAddItem();
+    }
+  })
+
 }(budgetController, UIController));
 
 

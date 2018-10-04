17eb549772ac61ccdfde8044a0173ca5d4926092
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-10-04 14:23:39 +0100

    add UIcontroller query selectors

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
+  return {
+    getInput: function(){
+      return {
+        type: document.querySelector('.add__type').value,
+        description: document.querySelector('.add__description').value,
+        value: document.querySelector('.add__value').value
+      };
+    }
+  };
 
 }());
 
@@ -18,6 +27,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var ctrlAddItem = function(){
     // 1. Get field input data
+    var input = UIController.getInput();
+    console.log(input);
     // 2, Add item to the budget controller
     // 3. Add items to UI
     // 4. Calculate the budget

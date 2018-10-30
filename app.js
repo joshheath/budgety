ae20743cd3e817bde41664ee23991a3c8c3887de
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-10-30 11:41:23 -0500

    add deleteItem method

--- app.js
+++ app.js
@@ -58,6 +58,21 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id){
+      var ids, index;
+
+      ids = data.allItems[type].map(function(current){
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index != -1) {
+        data.allItems[type]splice(index, 1);
+      }
+
+    },
+
     calculateBudget: function() {
       // Calculate total income and expenses
       calculateTotal('exp');

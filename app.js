a56a7721ca07b4d9f52ba328cbe9c6aacc15a98d
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-11-06 16:21:18 -0500

    add calc and get percentage functons to expenses prototype

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ var budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
+  };
+
+  Expense.prototype.calcPercentage = function(totalIncome){
+    if (totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+    } else {
+      this.percentage = -1;
+    }
+  };
+
+  Expense.prototype.getPercentage = function(){
+    return this.percentage;
   };
 
   var Income = function(id, description, value) {
@@ -88,6 +101,14 @@ var budgetController = (function() {
       } else {
         data.percentage = -1;
       }
+    },
+
+
+    calculatePercentages: function(){
+
+
+
+
     },
 
     getBudget: function(){
